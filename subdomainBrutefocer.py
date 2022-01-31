9d3220bc2f6ef63fdd420198f2414d7d4a3e3499
captaininappropriate <>
2022-01-31 12:52:01 +1100

    new file created

---
+++ subdomainBrutefocer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Description   :  
+# Version       :
+# Author        :
+
+# take a text file of subdomains and domain name as input
+# validate supplied file
+
+# check for wildcard response using random subdomain followed by domain name target
+
+# for each subdomain do a lookup and check the response
+# for all subdomains which provide a valid response add to a file for output
+
+# import required modules
+
